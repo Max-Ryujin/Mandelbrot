71812baf6e083030a75f28243e1e4b6e280ac9e6
Max-Ryujin <61623298+Max-Ryujin@users.noreply.github.com>
2020-02-29 14:10:39 +0100

    Initial commit

---
+++ Mandelbrot/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Mandelbrot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1900, 1900);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1968, 1384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 115);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2157, 1083);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "300";
+          
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(2157, 1145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "200";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(2157, 1203);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 31);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1974, 1089);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "vergrößerung";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2008, 1338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "iterationen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2015, 1279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "auflösung";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1963, 1209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "yverschiebung";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1963, 1145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "xverschiebung";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(2157, 1273);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 31);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "1000";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(2157, 1332);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 31);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Text = "100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2344, 1929);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Mandelbrot";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Mandelbrot/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+
+using System.Windows.Forms;
+
+namespace Mandelbrot
+{
+    public partial class Form1 : Form
+    {
+        int x, y;
+        int iteration = 100;
+        int vergrößerung = 2000;
+        double xverschiebung = -1000;
+        double yverschiebung = -1000;
+        int auflösung = 1000;
+        Bitmap map;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+    
+
+        public int mandelbrot(double x, double y, double m)
+        {
+            double xx = 0;
+            double yy = 0;
+            for (int i = 1; i <= iteration; i++)
+            {
+                double xtemp = xx;
+                xx = (xx * xx) - (yy* yy) + x;          
+                
+                yy = 2 * xtemp * yy + y;
+                if (betrag(xx, yy) > m)
+                {
+                    return (i%19)*40;
+                    
+                  
+                }
+            }
+           
+            return 765;
+        }
+
+        public double betrag(double x, double y)
+        {
+            return ((x* x) + (y* y));           
+        }
+        int xmin = 0;
+        int xmax = 99;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = 0;
+            y = 0;
+            xmin = 0;
+            xmax = 99;
+            vergrößerung = int.Parse( textBox1.Text);
+            xverschiebung = Double.Parse(textBox2.Text);
+            yverschiebung = Double.Parse(textBox3.Text);
+            auflösung = int.Parse(textBox4.Text);
+            map = new Bitmap(auflösung, auflösung);
+            iteration = int.Parse(textBox5.Text);
+            paint();
+        }
+
+       
+         public void paint()
+        {
+
+           
+            for (x = xmin; x <= xmax; x++)
+            {
+                for (y = 0; y < map.Height; y++)
+                {
+                    Color newColor;
+                    double xwert = ((x - (auflösung / 2.0)) / (vergrößerung * 100.0)) + (xverschiebung / 10.0);
+                    double ywert = ((y - (auflösung / 2.0)) / (vergrößerung * 100.0)) + (yverschiebung / 10.0);
+                    int value = mandelbrot(xwert, ywert, 50);
+                    if (value > 510)
+                    {
+                        newColor = Color.FromArgb(255, 255, value - 510);
+                    }
+                    else if (value > 255)
+                    {
+                        newColor = Color.FromArgb(255, value - 255, 0);
+                    }
+                    else
+                    {
+                        newColor = Color.FromArgb(value, 0, 0);
+                    }
+
+
+
+                    map.SetPixel(x, y, newColor);
+                }
+
+
+            }
+            xmin += 100;
+            xmax += 100;
+
+            pictureBox1.Image = map;
+            Refresh();
+            if (xmax < auflösung) paint();
+        }
+
+            
+        
+        
+    }   
+}
