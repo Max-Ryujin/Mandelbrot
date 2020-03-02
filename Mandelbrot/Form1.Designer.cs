e674f5ef593aaa6217ce98bd82e133e5c290e868
Max-Ryujin <61623298+Max-Ryujin@users.noreply.github.com>
2020-03-02 18:35:33 +0100

    UI

--- Mandelbrot/Form1.Designer.cs
+++ Mandelbrot/Form1.Designer.cs
@@ -50,18 +50,20 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1900, 1900);
+            this.pictureBox1.Size = new System.Drawing.Size(950, 988);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(2013, 1416);
+            this.button1.Location = new System.Drawing.Point(1006, 736);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(190, 97);
+            this.button1.Size = new System.Drawing.Size(95, 50);
             this.button1.TabIndex = 1;
             this.button1.Text = "Mandelbrot";
             this.button1.UseVisualStyleBackColor = true;
@@ -69,94 +71,105 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(2157, 1083);
+            this.textBox1.Location = new System.Drawing.Point(1078, 563);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "1";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(2157, 1145);
+            this.textBox2.Location = new System.Drawing.Point(1078, 595);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.Size = new System.Drawing.Size(52, 20);
             this.textBox2.TabIndex = 3;
             this.textBox2.Text = "0";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(2157, 1203);
+            this.textBox3.Location = new System.Drawing.Point(1078, 626);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 31);
+            this.textBox3.Size = new System.Drawing.Size(52, 20);
             this.textBox3.TabIndex = 4;
             this.textBox3.Text = "0";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1974, 1089);
+            this.label1.Location = new System.Drawing.Point(987, 566);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 25);
+            this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "vergrößerung";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(2008, 1338);
+            this.label2.Location = new System.Drawing.Point(1004, 696);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 25);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 6;
             this.label2.Text = "iterationen";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(2015, 1279);
+            this.label3.Location = new System.Drawing.Point(1008, 665);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(106, 25);
+            this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "auflösung";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1963, 1209);
+            this.label4.Location = new System.Drawing.Point(982, 629);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(152, 25);
+            this.label4.Size = new System.Drawing.Size(76, 13);
             this.label4.TabIndex = 8;
             this.label4.Text = "yverschiebung";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(1963, 1145);
+            this.label5.Location = new System.Drawing.Point(982, 595);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(152, 25);
+            this.label5.Size = new System.Drawing.Size(76, 13);
             this.label5.TabIndex = 9;
             this.label5.Text = "xverschiebung";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(2157, 1273);
+            this.textBox4.Location = new System.Drawing.Point(1078, 662);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 31);
+            this.textBox4.Size = new System.Drawing.Size(52, 20);
             this.textBox4.TabIndex = 10;
             this.textBox4.Text = "1000";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(2157, 1332);
+            this.textBox5.Location = new System.Drawing.Point(1078, 693);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 31);
+            this.textBox5.Size = new System.Drawing.Size(52, 20);
             this.textBox5.TabIndex = 11;
             this.textBox5.Text = "100";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(2013, 1547);
+            this.button2.Location = new System.Drawing.Point(1006, 804);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(190, 101);
+            this.button2.Size = new System.Drawing.Size(95, 53);
             this.button2.TabIndex = 12;
             this.button2.Text = "Julia";
             this.button2.UseVisualStyleBackColor = true;
@@ -165,11 +178,12 @@
             // trackBar1
             // 
             this.trackBar1.LargeChange = 1;
-            this.trackBar1.Location = new System.Drawing.Point(2288, 1523);
+            this.trackBar1.Location = new System.Drawing.Point(1144, 792);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.trackBar1.Maximum = 100;
             this.trackBar1.Minimum = -100;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(329, 90);
+            this.trackBar1.Size = new System.Drawing.Size(164, 45);
             this.trackBar1.TabIndex = 15;
             this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
             // 
@@ -177,19 +191,20 @@
             // 
             this.trackBar2.AllowDrop = true;
             this.trackBar2.LargeChange = 1;
-            this.trackBar2.Location = new System.Drawing.Point(2288, 1619);
+            this.trackBar2.Location = new System.Drawing.Point(1144, 842);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.trackBar2.Maximum = 100;
             this.trackBar2.Minimum = -100;
             this.trackBar2.Name = "trackBar2";
-            this.trackBar2.Size = new System.Drawing.Size(329, 90);
+            this.trackBar2.Size = new System.Drawing.Size(164, 45);
             this.trackBar2.TabIndex = 16;
             this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2683, 1929);
+            this.ClientSize = new System.Drawing.Size(1514, 965);
             this.Controls.Add(this.trackBar2);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button2);
@@ -205,6 +220,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Mandelbrot";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
